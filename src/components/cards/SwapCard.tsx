81eaef46b938d01a184aa41a83835e361ad00d79
Oleg Kirillovich <olegkron@ya.ru>
2023-05-12 21:27:10 +0100

    swap init

--- src/components/cards/SwapCard.tsx
+++ src/components/cards/SwapCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { CardHeader } from './CardHeader'
 import { Button } from '../buttons/Button/Button'
+import classNames from './SwapCard.module.pcss'
 
 interface SwapCardProps {}
 
@@ -8,17 +9,37 @@ export const SwapCard: FC<SwapCardProps> = () => {
   return (
     <div className="card">
       <CardHeader title={'Swap'}></CardHeader>
-      <Button primary lg leftIcon={{ name: 'ArrowsUpDown', iconProps: { size: 18 } }}>
-        Swap
-      </Button>
-      {/*<Tag*/}
-      {/*  sm*/}
-      {/*  primary*/}
-      {/*  fgColor={colors.green.main}*/}
-      {/*  bgColor={colors.green.darkest}*/}
-      {/*  leftIcon={{ name: 'ArrowUpRight', iconProps: { size: 18 } }}>*/}
-      {/*  +50%*/}
-      {/*</Tag>*/}
+
+      <div className={classNames.swapContainer}>
+        <div className={classNames.tokenContainer}>
+          <div className={classNames.tokenRow}>
+            <p>From</p>
+            <p>Max: 10</p>
+          </div>
+          <div className={classNames.tokenRow}>
+            <div>
+              <h3>40 BNB</h3>
+              <h5>$4024</h5>
+            </div>
+          </div>
+        </div>
+        <div className={classNames.tokenContainer}>
+          <div className={classNames.tokenRow}>
+            <p>To</p>
+            <p>Max: 10</p>
+          </div>
+          <div className={classNames.tokenRow}>
+            <div>
+              <h3>40 BNB</h3>
+              <h5>$4024</h5>
+            </div>
+          </div>
+        </div>
+
+        <Button primary lg leftIcon={{ name: 'ArrowsUpDown', iconProps: { size: 18 } }}>
+          Swap
+        </Button>
+      </div>
     </div>
   )
 }
