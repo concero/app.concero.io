8fc07d61ad1ae8fe856e137983dfdb1c06d091e2
Oleg Kron <olegkron@ya.ru>
2023-05-17 20:37:16 +0100

    Update rainbowKit.ts

--- src/web3/rainbowKit.ts
+++ src/web3/rainbowKit.ts
@@ -1,23 +1,14 @@
-import '@rainbow-me/rainbowkit/styles.css'
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createConfig, mainnet } from 'wagmi'
-
-import { publicProvider } from 'wagmi/providers/public'
-import { EthereumClient } from '@web3modal/ethereum'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon } from 'wagmi/chains'
 
 export const projectId = process.env.WALLETCONNECT_PROJECT_ID
 
-export const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet], [publicProvider()])
-
-export const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  projectId,
-  chains,
-})
-
+export const chains = [mainnet, polygon, arbitrum]
+export const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 export const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
   publicClient,
 })
 
