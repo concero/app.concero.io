93d800b6eea7145e4f1c8b8e2e9c5571cdbecc58
Oleg Kirillovich <olegkron@ya.ru>
2023-05-18 15:17:56 +0100

    lightwweight chart init

--- src/api/queue.ts
+++ src/api/queue.ts
@@ -1,18 +1,25 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-import axiosRetry from 'axios-retry'
 
 interface Request {
   config: AxiosRequestConfig
   resolve: (value?: AxiosResponse | PromiseLike<AxiosResponse>) => void
   reject: (reason?: any) => void
+  retries: number
 }
+
+const MAX_RETRIES = 2
 
 class Queue {
   private queue: Request[] = []
 
   async add(request: AxiosRequestConfig): Promise<AxiosResponse> {
     return new Promise((resolve, reject) => {
-      this.queue.push({ config: request, resolve, reject })
+      this.queue.push({
+        config: request,
+        resolve,
+        reject,
+        retries: 0,
+      })
       this.process()
     })
   }
@@ -20,22 +27,28 @@ class Queue {
   async process() {
     if (this.queue.length === 0) return
 
-    const { config, resolve, reject } = this.queue.shift()!
+    const { config, resolve, reject, retries } = this.queue.shift()!
 
     try {
       const response = await axios(config)
       resolve(response)
       this.process()
     } catch (error) {
-      reject(error)
-      // Retry the same request
-      this.queue.unshift({ config, resolve, reject })
-      this.process()
+      if (retries < MAX_RETRIES) {
+        // Retry the same request
+        this.queue.unshift({
+          config,
+          resolve,
+          reject,
+          retries: retries + 1,
+        })
+        this.process()
+      } else {
+        reject(error)
+      }
     }
   }
 }
-
-axiosRetry(axios, { retries: 3 })
 
 const queue = new Queue()
 
