4efafb082ec84f30bdc4d9bfc321b7e5d07da210
lufaque <95128636+lufaque@users.noreply.github.com>
2023-07-14 23:14:05 +0300

    chart localization

--- src/api/chart/getData.tsx
+++ src/api/chart/getData.tsx
@@ -1,13 +1,26 @@
 import { get } from '../clientProxy'
 
+const toLocalTime = (timestamp: string) => {
+  const currentTime = new Date()
+  // Получаем смещение текущего часового пояса в секундах
+  const timeZoneOffsetInSeconds = currentTime.getTimezoneOffset() * 60
+
+  return Number(timestamp) / 1000 - timeZoneOffsetInSeconds
+}
+
+interface Item {
+  0: string
+  1: number
+}
+
 export const getData = async (chain: string, currency: string, days: string) => {
   const url = `https://api.coingecko.com/api/v3/coins/${chain}/market_chart?vs_currency=${currency}&days=${days}`
   const response = await get(url)
 
   if (response.status === 200) {
-    return response.data.prices.map((item) => {
+    return response.data.prices.map((item: Item) => {
       return {
-        time: item[0] / 1000 + 3600,
+        time: toLocalTime(item[0]),
         value: item[1],
       }
     })
