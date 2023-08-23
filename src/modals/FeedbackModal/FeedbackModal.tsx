4b469ee58c6778a752089efafd2edbbf941f4ad0
Oleg Kirillovich <olegkron@ya.ru>
2023-08-23 23:44:00 +0100

    feedback form & button updates

--- src/modals/FeedbackModal/FeedbackModal.tsx
+++ src/modals/FeedbackModal/FeedbackModal.tsx
@@ -1,7 +1,6 @@
 import { FC, useState } from 'react'
 import classNames from './FeedbackModal.module.pcss'
 import { Modal } from '../../components/modals/Modal/Modal'
-import { Tag } from '../../components/tags/Tag/Tag'
 import { TextArea } from '../../components/layout/TextArea/TextArea'
 import { Button } from '../../components/buttons/Button/Button'
 
@@ -10,42 +9,46 @@ interface FeedbackModalProps {
   setShow: () => void
 }
 
+enum FeedbackTags {
+  QUESTION = 1,
+  ISSUE,
+  SUGGESTION,
+  LIKE,
+}
+
 export const FeedbackModal: FC<FeedbackModalProps> = ({ show, setShow }) => {
-  const [selectedTag, setSelectedTag] = useState('')
+  const [selectedTag, setSelectedTag] = useState<FeedbackTags | null>(null)
+
+  const feedbackOptions = [
+    { label: 'Question', value: FeedbackTags.QUESTION },
+    { label: 'Issue', value: FeedbackTags.ISSUE },
+    { label: 'Suggestion', value: FeedbackTags.SUGGESTION },
+    { label: 'I like something', value: FeedbackTags.LIKE },
+  ]
 
   return (
-    <Modal title={'Help us improve'} show={show} setShow={setShow}>
+    <Modal title="Help us improve" show={show} setShow={setShow}>
       <div className={classNames.container}>
         <div className={classNames.tagContainer}>
-          <Tag
-            color={selectedTag === 'question' ? 'main' : 'secondary'}
-            title={'Question'}
-            onClick={() => setSelectedTag('question')}
-          />
-          <Tag
-            color={selectedTag === 'issue' ? 'main' : 'secondary'}
-            title={'Issue'}
-            onClick={() => setSelectedTag('issue')}
-          />
-          <Tag
-            color={selectedTag === 'suggestion' ? 'main' : 'secondary'}
-            title={'Suggestion'}
-            onClick={() => setSelectedTag('suggestion')}
-          />
-          <Tag
-            color={selectedTag === 'i_like_something' ? 'main' : 'secondary'}
-            title={'I like something'}
-            onClick={() => setSelectedTag('i_like_something')}
-          />
+          {feedbackOptions.map((option) => (
+            <Button
+              key={option.value}
+              size="sm"
+              variant={selectedTag === option.value ? 'primary' : 'subtle'}
+              onClick={() => setSelectedTag(option.value)}
+            >
+              {option.label}
+            </Button>
+          ))}
         </div>
         <div className={classNames.textareaContainer}>
           <TextArea />
         </div>
         <Button
-          variant={'primary'}
+          variant="primary"
           leftIcon={{
-            name: 'ArrowsDiff',
-            iconProps: { transform: 'rotate(90)' },
+            name: 'Send',
+            iconProps: { size: 16 },
           }}
         >
           Send
