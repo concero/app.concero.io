3e0740a41690479d0b6e11031337a79dad32cc1c
lufaque <95128636+lufaque@users.noreply.github.com>
2023-08-29 21:16:58 +0300

    fetchStakingChartData.ts to concero backend

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 const client = axios.create({
   headers: {
@@ -8,22 +8,23 @@ const client = axios.create({
 
 const request = async (options: AxiosRequestConfig): Promise<any> => {
   try {
-    const response: AxiosResponse = await client(options)
-    return response
+    return await client(options)
   } catch (error) {
     console.error('Error', error.response)
-    throw error.response
+    throw error
   }
 }
 
-export const get = async (url: string, params?: any, headers?: any): Promise<any> => request({
+export const get = async (url: string, params?: any, headers?: any): Promise<any> =>
+  request({
     url,
     method: 'GET',
     params,
     headers,
   })
 
-export const post = async (url: string, data: any, headers?: any): Promise<any> => request({
+export const post = async (url: string, data: any, headers?: any): Promise<any> =>
+  request({
     url,
     method: 'POST',
     data,
--- src/api/defilama/fetchStakingChartData.ts
+++ src/api/defilama/fetchStakingChartData.ts
@@ -1,16 +1,15 @@
-import { pools } from '../../constants/pools'
-import { get } from '../client'
+import { get, post } from '../client'
 
-const getPoolIdByTokensAddresses = (tokensAddresses: string[]): string | undefined => {
-  const pool = pools.find((p) => {
-    return p.underlyingTokens?.some((address) => tokensAddresses.includes(address))
+const getPoolIdByTokensAddresses = async (tokensAddresses: string[]): string | undefined => {
+  const response = await post('http://localhost:4000/api/pools', {
+    underlyingTokens: tokensAddresses,
   })
-
-  return pool?.id
+  if (response.status !== 200) throw new Error('Error fetching data')
+  return response.data.data[0]?.pool
 }
 
 export const fetchStakingChartData = async (addresses: string[]) => {
-  const poolId = getPoolIdByTokensAddresses(addresses)
+  const poolId = await getPoolIdByTokensAddresses(addresses)
   if (!poolId) throw new Error('Pool not found')
   const response = await get(`https://yields.llama.fi/chart/${poolId}`)
   if (response.status !== 200) throw new Error('Error fetching data')
