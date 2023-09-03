257fbe3fcf464f37a94b218ac7fe1dbdd81a45b8
lufaque <95128636+lufaque@users.noreply.github.com>
2023-09-03 18:41:46 +0300

    fetch balance fixed, packages updated

--- src/components/cards/SwapCard/handlers/handleBalance.ts
+++ src/components/cards/SwapCard/handlers/handleBalance.ts
@@ -21,6 +21,7 @@ interface HandleBalanceProps {
 }
 
 export const handleBalance = async ({ swapDispatch, from, address }: HandleBalanceProps) => {
+  if (!address) return swapDispatch({ type: 'SET_BALANCE', payload: null })
   const response = await fetchTokenBalance(from.chain.providers.rango.key, from.token.address, address, from.token.symbol)
   const result = response?.data ? `${addingTokenDecimals(Number(response.data), from.token.decimals)} ${from.token.symbol}` : null
   swapDispatch({ type: 'SET_BALANCE', payload: result })
