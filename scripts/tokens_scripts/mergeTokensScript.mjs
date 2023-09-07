c6142540f6f7d033d49b16ec41b02d8b8d1fd8e8
lufaque <95128636+lufaque@users.noreply.github.com>
2023-09-07 19:52:29 +0300

    fix

--- scripts/tokens_scripts/mergeTokensScript.mjs
+++ scripts/tokens_scripts/mergeTokensScript.mjs
@@ -1,56 +1,56 @@
-import fs from 'fs';
-import {lifiTokens} from './lifiTokens.mjs';
-import {rangoTokens} from './rangoTokens.mjs';
+import fs from 'fs'
+import { lifiTokens } from './lifiTokens.mjs'
+import { rangoTokens } from './rangoTokens.mjs'
 
 function mergeTokens(lifiTokens, rangoTokens) {
-    const mergedTokens = {};
+  const mergedTokens = {}
 
-    for (const chainId in lifiTokens) {
-        const lifiChainTokens = lifiTokens[chainId];
-        const mergedChainTokens = [];
+  for (const chainId in lifiTokens) {
+    const lifiChainTokens = lifiTokens[chainId]
+    const mergedChainTokens = []
 
-        for (const token of lifiChainTokens) {
-            mergedChainTokens.push({
-                name: token.name,
-                symbol: token.symbol,
-                address: token.address,
-                decimals: token.decimals,
-                logoURI: token.logoURI,
-                providers: ["lifi"]
-            });
-        }
-
-        mergedTokens[chainId] = mergedChainTokens;
+    for (const token of lifiChainTokens) {
+      mergedChainTokens.push({
+        name: token.name,
+        symbol: token.symbol,
+        address: token.address,
+        decimals: token.decimals,
+        logoURI: token.logoURI,
+        providers: ['lifi'],
+      })
     }
 
-    rangoTokens.forEach(token => {
-        if (!mergedTokens[token.chainId]) {
-            mergedTokens[token.chainId] = [];
-        }
+    mergedTokens[chainId] = mergedChainTokens
+  }
 
-        const mergedToken = {
-            name: token.name,
-            symbol: token.symbol,
-            address: token.address,
-            decimals: token.decimals,
-            logoURI: token.image,
-            providers: ["rango"]
-        }
+  rangoTokens.forEach((token) => {
+    if (!mergedTokens[token.chainId]) {
+      mergedTokens[token.chainId] = []
+    }
 
-        const existingToken = mergedTokens[token.chainId].find(t => t.address === mergedToken.address);
+    const mergedToken = {
+      name: token.name,
+      symbol: token.symbol,
+      address: token.address,
+      decimals: token.decimals,
+      logoURI: token.image,
+      providers: ['rango'],
+    }
 
-        if (existingToken) {
-            if (!existingToken.providers.includes("rango")) existingToken.providers?.push("rango");
-        } else {
-            mergedTokens[token.chainId].push(mergedToken);
-        }
-    });
+    const existingToken = mergedTokens[token.chainId].find((t) => t.address === mergedToken.address)
 
-    return mergedTokens;
+    if (existingToken && !existingToken.providers.includes('rango')) {
+      existingToken.providers?.push('rango')
+    } else {
+      mergedTokens[token.chainId].push(mergedToken)
+    }
+  })
+
+  return mergedTokens
 }
 
-const mergedTokens = mergeTokens(lifiTokens, rangoTokens);
-const mergedTokensString = `export const tokens = ${JSON.stringify(mergedTokens, null, 4)}`;
-fs.writeFileSync('tokens.ts', mergedTokensString, 'utf-8');
+const mergedTokens = mergeTokens(lifiTokens, rangoTokens)
+const mergedTokensString = `export const tokens = ${JSON.stringify(mergedTokens, null, 4)}`
+fs.writeFileSync('tokens.ts', mergedTokensString, 'utf-8')
 
-console.log('Merged tokens data has been written to tokens.js');
+console.log('Merged tokens data has been written to tokens.js')
