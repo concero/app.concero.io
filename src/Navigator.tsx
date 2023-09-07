876cfbaa1d020fb5c4ba967752ebb1717bb5e93d
Oleg Kirillovich <olegkron@ya.ru>
2023-09-07 14:32:06 +0100

    rm staking Screen from master

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -5,32 +5,30 @@ import { AppScreen } from './components/screens/AppScreen/AppScreen'
 import { Header } from './components/layout/Header/Header/Header'
 import { routes } from './constants/routes'
 import { PortfolioScreen } from './components/screens/PortfolioScreen/PortfolioScreen'
-import { StakingScreen } from './components/screens/StakingScreen/StakingScreen'
 
 export interface NavigatorProps {}
 
-export const Navigator: FC<NavigatorProps> = ({}) =>
+export const Navigator: FC<NavigatorProps> = ({}) => (
   // const { modalState, modalDispatch } = useContext(ModalContext)
-   (
-     <BrowserRouter>
-       <AppScreen>
-         <Header />
-         <Routes>
-           <Route path={routes.exchange} element={<ExchangeScreen />} />
-           <Route path={routes.portfolio} element={<PortfolioScreen />} />
-           <Route path={routes.staking} element={<StakingScreen />} />
-           <Route path={routes.root} element={<Navigate to={routes.exchange} />} />
-         </Routes>
-         {/* todo: : may be a good idea to have 1 top level modal with context, issue is - everything rerenders when modal is open */}
-         {/* <EntityListModal */}
-         {/*  animate={false} */}
-         {/*  title={`Select ${modalState.entityType}`} */}
-         {/*  data={modalState.data} */}
-         {/*  columns={modalState.entityType === 'chain' ? ChainColumns : TokenColumns} */}
-         {/*  show={modalState.show} */}
-         {/*  setShow={(value) => modalDispatch({ type: 'SET_SHOW', payload: value })} */}
-         {/*  onSelect={(entity) => modalDispatch({ type: 'SET_ENTITY', payload: entity })} */}
-         {/* /> */}
-       </AppScreen>
-     </BrowserRouter>
-  )
+  <BrowserRouter>
+    <AppScreen>
+      <Header />
+      <Routes>
+        <Route path={routes.exchange} element={<ExchangeScreen />} />
+        <Route path={routes.portfolio} element={<PortfolioScreen />} />
+        {/* <Route path={routes.staking} element={<StakingScreen />} /> */}
+        <Route path={routes.root} element={<Navigate to={routes.exchange} />} />
+      </Routes>
+      {/* todo: : may be a good idea to have 1 top level modal with context, issue is - everything rerenders when modal is open */}
+      {/* <EntityListModal */}
+      {/*  animate={false} */}
+      {/*  title={`Select ${modalState.entityType}`} */}
+      {/*  data={modalState.data} */}
+      {/*  columns={modalState.entityType === 'chain' ? ChainColumns : TokenColumns} */}
+      {/*  show={modalState.show} */}
+      {/*  setShow={(value) => modalDispatch({ type: 'SET_SHOW', payload: value })} */}
+      {/*  onSelect={(entity) => modalDispatch({ type: 'SET_ENTITY', payload: entity })} */}
+      {/* /> */}
+    </AppScreen>
+  </BrowserRouter>
+)
