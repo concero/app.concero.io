324d2a61022d7155ab3fe24394b9ea507f915329
Oleg Kirillovich <olegkron@ya.ru>
2023-09-10 11:39:25 +0100

    bitquery

--- src/api/concero/fetchTokensByChainId.ts
+++ src/api/concero/fetchTokensByChainId.ts
@@ -1,7 +1,8 @@
 import { get } from '../client'
 
 export const fetchTokensByChainId = async (chainId: string) => {
-  const url = `http://localhost:4000/api/tokens/${chainId}`
+  console.log('fetchTokensByChainId')
+  const url = `http://localhost:4000/api/tokens?chain_id=${chainId}`
   const response = await get(url)
   if (response.status !== 200) throw new Error(response.statusText)
   return response.data.data
--- src/components/cards/SwapCard/TokenArea/getTokens.ts
+++ src/components/cards/SwapCard/TokenArea/getTokens.ts
@@ -2,7 +2,9 @@ import { fetchTokensByChainId } from '../../../../api/concero/fetchTokensByChain
 
 export const getTokens = async (selection, tokenAreaDispatch) => {
   try {
+    console.log('getTokens')
     const response = await fetchTokensByChainId(selection.chain.id)
+    console.log(response)
     tokenAreaDispatch({ type: 'SET_TOKENS', payload: response })
   } catch (error) {
     console.log(error)
