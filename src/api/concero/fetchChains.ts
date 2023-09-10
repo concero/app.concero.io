ef7161ab57c831318f9bc4738af8a069657ef7a7
Oleg Kirillovich <olegkron@ya.ru>
2023-09-10 14:34:12 +0100

    api concero address fix

--- src/api/concero/fetchChains.ts
+++ src/api/concero/fetchChains.ts
@@ -2,7 +2,7 @@ import { get } from '../client'
 
 // TODO add other props to the fetchChains function
 export const fetchChains = async (chainId = null) => {
-  const url = `http://localhost:4000/api/chains${chainId ? `?id=${chainId}` : ''}`
+  const url = `${process.env.CONCERO_API_URL}/chains${chainId ? `?id=${chainId}` : ''}`
   const response = await get(url)
   if (response.status !== 200) throw new Error('no chains found')
   return response.data.data
