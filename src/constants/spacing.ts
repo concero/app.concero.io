317a439370cf6cb67af359e5e5a157d93def4d74
Oleg Kirillovich <olegkron@ya.ru>
2023-09-11 18:46:21 +0100

    coingecko ids to tokens

--- src/constants/spacing.ts
+++
@@ -1,6 +0,0 @@
-import spacingJson from './json/spacing.json'
-import { NestedObj } from '../types/utils'
-
-type SpacingKeys = keyof typeof spacingJson.sp
-type Spacing = NestedObj<typeof spacingJson.sp, SpacingKeys>
-export const sp = spacingJson.sp as Spacing
--- src/constants/styles.ts
+++
@@ -1,6 +0,0 @@
-import stylesJson from './json/styles.json'
-import { NestedObj } from '../types/utils'
-
-type stylesKeys = keyof typeof stylesJson.st
-type styles = NestedObj<typeof stylesJson.st, stylesKeys>
-export const sp = stylesJson.st as styles
