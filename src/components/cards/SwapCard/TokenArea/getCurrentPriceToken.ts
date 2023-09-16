51b1e6ba462da56925a0795be8b7d595fe0e58dd
lufaque <95128636+lufaque@users.noreply.github.com>
2023-09-16 19:44:21 +0300

    fix

--- src/components/cards/SwapCard/TokenArea/getCurrentPriceToken.ts
+++ src/components/cards/SwapCard/TokenArea/getCurrentPriceToken.ts
@@ -1,8 +1,6 @@
 import { fetchCurrentTokenPriceUSD } from '../../../../api/coinGecko/fetchCurrentTokenPriceUSD'
 
 export const getCurrentPriceToken = async (selection, tokenAreaDispatch) => {
-  console.log('SELECTION: ', selection)
-
   try {
     const response = await fetchCurrentTokenPriceUSD(selection.token.coinGeckoId)
     tokenAreaDispatch({ type: 'SET_CURRENT_TOKEN_PRICE_USD', payload: response })
