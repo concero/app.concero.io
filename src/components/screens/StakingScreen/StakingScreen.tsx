978a7839fc91efe2c029bfd3d2ed84635655b0ef
lufaque <95128636+lufaque@users.noreply.github.com>
2023-09-16 16:06:13 +0300

    StakingScreen.tsx refactor

--- src/components/screens/StakingScreen/StakingScreen.tsx
+++ src/components/screens/StakingScreen/StakingScreen.tsx
@@ -1,19 +1,11 @@
 import { FC, useContext, useEffect } from 'react'
 import { useAccount } from 'wagmi'
-import classNames from './StakingScreen.module.pcss'
-import { StakingOpportunitiesCard } from '../../cards/StakingOpportunitesCard/StakingOpportunitiesCard'
-import { StakingHeaderCard } from '../../cards/StakingHeaderCard/StakingHeaderCard'
-import { StakingChartCard } from '../../cards/StakingChartCard/StakingChartCard'
-import { StakingHighlightsCard } from '../../cards/StakingHighlightsCard/StakingHighlightsCard'
-import { RatioCard } from '../../cards/RatioCard/RatioCard'
-import { DetailsCard } from '../../cards/DetailsCard/DetailsCard'
 import { useStakingReducer } from './stakingReducer/stakingReducer'
 import { useMediaQuery } from '../../../hooks/useMediaQuery'
 import { DataContext } from '../../../hooks/DataContext/DataContext'
 import { populateChains } from './populateFunctions'
-import { TokensCard } from '../../cards/TokensCard/TokensCard'
-import { RewardsCard } from '../../cards/RewardsCard/RewardsCard'
-import { PredictionCard } from '../../cards/PredictionCard/PredictionCard'
+import { DesktopLayout } from './DesktopLayout'
+import { MobileLayout } from './MobileLayout'
 
 export const StakingScreen: FC = () => {
   const { getChains } = useContext(DataContext)
@@ -23,42 +15,12 @@ export const StakingScreen: FC = () => {
 
   useEffect(() => {
     populateChains(getChains, dispatch)
-    dispatch({ type: 'SET_ADDRESS', payload: address })
+    if (address) dispatch({ type: 'SET_ADDRESS', payload: address })
   }, [])
 
-  const mobileLayout = (
-    <div className={classNames.container}>
-      <StakingOpportunitiesCard stakingState={stakingState} dispatch={dispatch} />
-      {stakingState.selectedVault ? (
-        <div className={classNames.mainCardStack}>
-          <StakingChartCard stakingState={stakingState} />
-          <StakingHighlightsCard stakingState={stakingState} />
-          <RatioCard />
-          <DetailsCard />
-        </div>
-      ) : null}
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      {isDesktop ? <DesktopLayout stakingState={stakingState} dispatch={dispatch} /> : <MobileLayout stakingState={stakingState} dispatch={dispatch} />}
     </div>
   )
-
-  const desktopLayout = (
-    <div className={classNames.container}>
-      <StakingOpportunitiesCard stakingState={stakingState} dispatch={dispatch} />
-      {stakingState.selectedVault ? (
-        <div className={classNames.stacksContainer}>
-          <div className={classNames.mainCardStack}>
-            <StakingHeaderCard stakingState={stakingState} />
-            <StakingChartCard stakingState={stakingState} />
-          </div>
-          <div className={`card ${classNames.secondaryCardStack}`}>
-            <StakingHighlightsCard stakingState={stakingState} />
-            <TokensCard stakingState={stakingState} />
-            <RewardsCard stakingState={stakingState} />
-            <PredictionCard stakingState={stakingState} />
-          </div>
-        </div>
-      ) : null}
-    </div>
-  )
-
-  return <div style={{ width: '100%', height: '100%' }}>{isDesktop ? desktopLayout : mobileLayout}</div>
 }
