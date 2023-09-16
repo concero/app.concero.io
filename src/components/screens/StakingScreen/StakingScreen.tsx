d0440c929af3bc2105f4f6b9f727949070254f6c
lufaque <95128636+lufaque@users.noreply.github.com>
2023-09-16 16:50:47 +0300

    wido first step

--- src/components/screens/StakingScreen/StakingScreen.tsx
+++ src/components/screens/StakingScreen/StakingScreen.tsx
@@ -1,4 +1,5 @@
 import { FC, useContext, useEffect } from 'react'
+import { approve } from 'wido'
 import { useAccount } from 'wagmi'
 import { useStakingReducer } from './stakingReducer/stakingReducer'
 import { useMediaQuery } from '../../../hooks/useMediaQuery'
@@ -16,7 +17,17 @@ export const StakingScreen: FC = () => {
   useEffect(() => {
     populateChains(getChains, dispatch)
     if (address) dispatch({ type: 'SET_ADDRESS', payload: address })
+    getApproveData()
   }, [])
+
+  async function getApproveData() {
+    const { data, to } = await approve({
+      chainId: 1,
+      fromToken: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      toToken: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      amount: '1000000000000000000',
+    })
+  }
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
