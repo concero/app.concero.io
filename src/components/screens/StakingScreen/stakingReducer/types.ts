ca0144c805296902708794c14d69bd60720b0da3
lufaque <95128636+lufaque@users.noreply.github.com>
2023-10-06 20:35:49 +0300

    my position filter added

--- src/components/screens/StakingScreen/stakingReducer/types.ts
+++ src/components/screens/StakingScreen/stakingReducer/types.ts
@@ -5,101 +5,101 @@ export type Filter = {
 	search: string
 	all: boolean
 	my_holdings: boolean
-	chains: string[]
+	my_positions: boolean
+	chains: Chain[]
 	apy: string
 	category: string[]
 }
 
-
 interface ITokenInfo {
-  _id: string
-  name: string
-  symbol: string
-  logoURI: string
+	_id: string
+	name: string
+	symbol: string
+	logoURI: string
 }
 
 interface IPredictions {
-  predictedClass: string
-  predictedProbability: number
-  binnedConfidence: number
+	predictedClass: string
+	predictedProbability: number
+	binnedConfidence: number
 }
 
 interface IData {
-  category?: string
-  symbols?: string[]
-  vault_token?: {
-    address?: string
-    name?: string
-    symbol?: string
-    decimals?: number
-  }
-  project_id?: string
-  project_name?: string
-  chain_name?: string
-  apy?: number
-  apyBase?: number
-  apyReward?: number
-  apyPct1D?: number
-  apyPct7D?: number
-  apyPct30D?: number
-  apyMean30d?: number
-  apyBase7d?: number
-  volumeUsd1d?: number
-  volumeUsd7d?: number
-  tvlUsd?: number
-  is_stablecoin?: boolean
-  il_risk?: string
-  exposure?: string
-  predictions?: IPredictions
-  mu?: string
-  sigma?: string
-  count?: string
-  is_outlier?: boolean
-  il7d?: string
-  apyBaseInception?: string
-  defiLlamaPoolId?: string
-  defiLlamaUnderlyingTokens?: string[]
-  defiLlamaRewardTokens?: string[]
-  underlyingTokens?: string[]
-  subtitle?: string
-  chain: {
-    _id?: string
-    id?: string
-    name?: string
-    symbol?: string
-    logoURI?: string
-  }
+	category?: string
+	symbols?: string[]
+	vault_token?: {
+		address?: string
+		name?: string
+		symbol?: string
+		decimals?: number
+	}
+	project_id?: string
+	project_name?: string
+	chain_name?: string
+	apy?: number
+	apyBase?: number
+	apyReward?: number
+	apyPct1D?: number
+	apyPct7D?: number
+	apyPct30D?: number
+	apyMean30d?: number
+	apyBase7d?: number
+	volumeUsd1d?: number
+	volumeUsd7d?: number
+	tvlUsd?: number
+	is_stablecoin?: boolean
+	il_risk?: string
+	exposure?: string
+	predictions?: IPredictions
+	mu?: string
+	sigma?: string
+	count?: string
+	is_outlier?: boolean
+	il7d?: string
+	apyBaseInception?: string
+	defiLlamaPoolId?: string
+	defiLlamaUnderlyingTokens?: string[]
+	defiLlamaRewardTokens?: string[]
+	underlyingTokens?: string[]
+	subtitle?: string
+	chain: {
+		_id?: string
+		id?: string
+		name?: string
+		symbol?: string
+		logoURI?: string
+	}
 }
 interface IProviderInfo {
-  id?: string
-  poolAddress?: string
-  primaryAddress?: string
-  tokenAddress?: string
+	id?: string
+	poolAddress?: string
+	primaryAddress?: string
+	tokenAddress?: string
 }
 
 export interface Vault {
-  _id: string
-  address?: string
-  address_normalized?: string
-  chain_id?: string
-  decimals?: number
-  symbol?: string
-  name?: string
-  input_tokens?: ITokenInfo[]
-  reward_tokens?: ITokenInfo[]
-  data?: IData
-    project: {
-    _id?: string
-    name?: string
-    logoURI?: string
-    category?: string
-  }
-  providers?: {
-    enso?: IProviderInfo
-    wido?: IProviderInfo
-  }
-  // front-end
-  stakedAmount?: string
+	_id: string
+	address?: string
+	address_normalized?: string
+	chain_id?: string
+	decimals?: number
+	symbol?: string
+	name?: string
+	input_tokens?: ITokenInfo[]
+	reward_tokens?: ITokenInfo[]
+	data?: IData
+	project: {
+		_id?: string
+		name?: string
+		logoURI?: string
+		category?: string
+	}
+	providers?: {
+		enso?: IProviderInfo
+		wido?: IProviderInfo
+	}
+	// front-end
+	stakedAmount?: string
 }
 
 export type InputTokens = {
@@ -112,6 +112,7 @@ export type InputTokens = {
 export enum FilterCategory {
 	all = 'all',
 	my_holdings = 'my_holdings',
+	my_positions = 'my_positions',
 	chains = 'chains',
 	apy = 'apy',
 	category = 'category',
