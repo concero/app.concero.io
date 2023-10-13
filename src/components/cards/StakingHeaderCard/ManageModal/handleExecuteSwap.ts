811d1a2d05ba30abd9e7979640c03fea84fbd4d2
lufaque <95128636+lufaque@users.noreply.github.com>
2023-10-13 19:01:21 +0300

    execution fix

--- src/components/cards/StakingHeaderCard/ManageModal/handleExecuteSwap.ts
+++ src/components/cards/StakingHeaderCard/ManageModal/handleExecuteSwap.ts
@@ -3,6 +3,9 @@ import { ManageAction, ManageState } from './useManageReducer/types'
 import { Status } from './constants'
 import { getSigner } from '../../../../web3/getSigner'
 import { SwitchNetworkArgs, SwitchNetworkResult } from '@wagmi/core'
+import { Contract } from 'ethers'
+import { JsonRpcSigner } from '@ethersproject/providers/src.ts/json-rpc-provider'
+import BigNumber from 'bignumber.js'
 
 function handleError(error: Error, manageDispatch: Dispatch<ManageAction>): void {
 	if (error.message.includes('INSUFFICIENT_GAS_TOKENS')) {
@@ -16,22 +19,29 @@ function handleError(error: Error, manageDispatch: Dispatch<ManageAction>): void
 
 type SwitchChainNetwork = (chainId_?: SwitchNetworkArgs['chainId']) => Promise<SwitchNetworkResult>
 
+async function approveToken(signer: JsonRpcSigner, receiverAddress: string, fromTokenAddress: string, fromAmount: string): Promise<void> {
+	const erc20 = new Contract(fromTokenAddress, ['function approve(address,uint256)'], signer)
+	return (await erc20.approve(receiverAddress, fromAmount)).wait()
+}
+
 export async function handleExecuteSwap(manageState: ManageState, manageDispatch: Dispatch<ManageAction>, switchNetworkAsync: SwitchChainNetwork): Promise<void> {
 	manageDispatch({ type: 'SET_LOADING', payload: true })
 	manageDispatch({ type: 'SET_STATUS', payload: Status.loading })
 
 	if (!manageState.route) return
+	const { from } = manageState
 
 	try {
-		const { from } = manageState
 		const signer = await getSigner(Number(from.chain.id), switchNetworkAsync)
+		await approveToken(signer, manageState.address, from.token.address, from.amount)
 
 		const transactionArgs = {
 			...manageState.route.tx,
-			// gasLimit: BigNumber(manageState.route.gas).times(1.2).toFixed(0).toString(),
+			gasLimit: BigNumber(manageState.route.gas).times(1.1).toFixed(0).toString(),
 		}
 
 		console.log('transactionArgs: ', transactionArgs)
+
 		await signer.sendTransaction(transactionArgs)
 		manageDispatch({ type: 'SET_STATUS', payload: Status.success })
 	} catch (error) {
--- src/web3/getSigner.ts
+++ src/web3/getSigner.ts
@@ -1,10 +1,11 @@
 import { createWalletClient, custom } from 'viem'
 import { providers } from 'ethers'
 import { SwitchNetworkArgs, SwitchNetworkResult } from '@wagmi/core'
+import { JsonRpcSigner } from '@ethersproject/providers/src.ts/json-rpc-provider'
 
 type SwitchChainNetwork = (chainId_?: SwitchNetworkArgs['chainId']) => Promise<SwitchNetworkResult>
 
-export async function getSigner(requiredChainId: number, switchNetworkAsync: SwitchChainNetwork) {
+export async function getSigner(requiredChainId: number, switchNetworkAsync: SwitchChainNetwork): Promise<JsonRpcSigner> {
 	if (Number(window.ethereum.chainId) !== requiredChainId && switchNetworkAsync) {
 		await switchNetworkAsync(requiredChainId)
 	}
