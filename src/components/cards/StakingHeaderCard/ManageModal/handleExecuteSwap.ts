ba2ab8f835514479170c04a4673fcdd06eeadfff
lufaque <95128636+lufaque@users.noreply.github.com>
2023-10-13 15:57:45 +0300

    execution fix

--- src/components/cards/StakingHeaderCard/ManageModal/handleExecuteSwap.ts
+++ src/components/cards/StakingHeaderCard/ManageModal/handleExecuteSwap.ts
@@ -3,7 +3,6 @@ import { ManageAction, ManageState } from './useManageReducer/types'
 import { Status } from './constants'
 import { getSigner } from '../../../../web3/getSigner'
 import { SwitchNetworkArgs, SwitchNetworkResult } from '@wagmi/core'
-import { BigNumber } from 'bignumber.js'
 
 function handleError(error: Error, manageDispatch: Dispatch<ManageAction>): void {
 	if (error.message.includes('INSUFFICIENT_GAS_TOKENS')) {
@@ -29,7 +28,7 @@ export async function handleExecuteSwap(manageState: ManageState, manageDispatch
 
 		const transactionArgs = {
 			...manageState.route.tx,
-			gasLimit: BigNumber(manageState.route.gas).times(1.2).toFixed(0).toString(),
+			// gasLimit: BigNumber(manageState.route.gas).times(1.2).toFixed(0).toString(),
 		}
 
 		console.log('transactionArgs: ', transactionArgs)
