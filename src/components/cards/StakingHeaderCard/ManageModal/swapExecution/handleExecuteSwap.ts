5ba1085f36548c1d45e4179d1c67a736333a6baa
lufaque <95128636+lufaque@users.noreply.github.com>
2023-10-17 12:05:17 +0300

    refactor

--- src/components/cards/StakingHeaderCard/ManageModal/swapExecution/handleExecuteSwap.ts
+++ src/components/cards/StakingHeaderCard/ManageModal/swapExecution/handleExecuteSwap.ts
@@ -1,47 +1,16 @@
 import { Dispatch } from 'react'
-import { ManageAction, ManageState } from './useManageReducer/types'
-import { Status } from './constants'
-import { getSigner } from '../../../../web3/getSigner'
+import { ManageAction, ManageState } from '../useManageReducer/types'
+import { Status } from '../constants'
+import { getSigner } from '../../../../../web3/getSigner'
 import { SwitchNetworkArgs, SwitchNetworkResult } from '@wagmi/core'
-import { Contract } from 'ethers'
 import BigNumber from 'bignumber.js'
-import { addingAmountDecimals } from '../../../../utils/formatting'
-import { fetchApprovalTx } from '../../../../api/enso/approvalTx'
-import { retryRequest } from '../../../../utils/retryRequest'
-import { fetchEnsoRoute } from '../../../../api/enso/fetchEnsoQuote'
-import { get } from '../../../../api/client'
-
-function handleError(error: Error, manageDispatch: Dispatch<ManageAction>): void {
-	if (error.message.includes('INSUFFICIENT_GAS_TOKENS')) {
-		manageDispatch({ type: 'SET_STATUS', payload: Status.balanceError })
-	} else if (error.message.toLowerCase().includes('user rejected')) {
-		manageDispatch({ type: 'SET_STATUS', payload: Status.canceled })
-	} else {
-		manageDispatch({ type: 'SET_STATUS', payload: Status.unknownError })
-	}
-}
+import { addingAmountDecimals } from '../../../../../utils/formatting'
+import { fetchApprovalTx } from '../../../../../api/enso/approvalTx'
+import { retryRequest } from '../../../../../utils/retryRequest'
+import { fetchEnsoRoute } from '../../../../../api/enso/fetchEnsoQuote'
+import { approveToken, checkIsApproveNeeded, handleError } from './helpers'
 
 type SwitchChainNetwork = (chainId_?: SwitchNetworkArgs['chainId']) => Promise<SwitchNetworkResult>
-
-async function approveToken({ signer, tokenAddress, receiverAddress, fromAmount }): Promise<void> {
-	console.log('approveToken', receiverAddress, tokenAddress, fromAmount)
-	const erc20 = new Contract(tokenAddress, ['function approve(address,uint256)'], signer)
-	return (await erc20.approve(receiverAddress, fromAmount)).wait()
-}
-
-interface IApproval {
-	allowance: string
-	spender: string
-	token: string
-}
-
-async function checkIsApproveNeeded(fromChainId: number, fromAddress: string, tokenAddress: string, fromAmount: string): Promise<boolean> {
-	const url = `https://api.enso.finance/api/v1/wallet/approvals?chainId=${fromChainId}&fromAddress=${fromAddress}`
-	const response = await get(url)
-	const approval: IApproval | undefined = response.data.find((item: IApproval) => item.token === tokenAddress)
-	if (!approval) return true
-	return new BigNumber(approval.allowance).isLessThan(fromAmount)
-}
 
 export async function handleExecuteSwap(manageState: ManageState, manageDispatch: Dispatch<ManageAction>, switchNetworkAsync: SwitchChainNetwork): Promise<void> {
 	manageDispatch({ type: 'SET_LOADING', payload: true })
