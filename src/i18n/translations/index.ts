7e6a4c29d819206d862f268596af37782941f1ac
lufaque <95128636+lufaque@users.noreply.github.com>
2023-11-11 17:00:50 +0300

    fr, pt, ko, id, it

--- src/i18n/translations/index.ts
+++ src/i18n/translations/index.ts
@@ -1,7 +1,17 @@
 import { en } from './en'
 import { es } from './es'
+import { fr } from './fr'
+import { id } from './id'
+import { it } from './it'
+import { ko } from './ko'
+import { pt } from './pt'
 
 export const translations = {
 	en: en,
 	es: es,
+	fr: fr,
+	id: id,
+	it: it,
+	ko: ko,
+	pt: pt,
 }
