e7bff888c04e0986097ed78a7a72a3ff29d57d89
lufaque <95128636+lufaque@users.noreply.github.com>
2023-11-18 21:13:27 +0300

    ListModal.test.tsx, SwapButton.test.tsx

---
+++ src/__tests__/components/ListModal.test.tsx
@@ -0,0 +1,36 @@
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { ListModal } from '../../components/modals/ListModal/ListModal'
+import jest from 'jest-mock'
+
+const mockGetItems = jest.fn().mockResolvedValue([])
+
+describe('ListModal Component', () => {
+	it('renders ListModal component correctly', () => {
+		const { getByText, getByPlaceholderText } = render(
+			<ListModal getItems={mockGetItems} isOpen={true} setIsOpen={() => {}} title="Test Modal" RenderItem={() => <div>Mock Item</div>} />,
+		)
+
+		expect(getByText('Test Modal')).toBeInTheDocument()
+		expect(getByPlaceholderText('Search...')).toBeInTheDocument()
+	})
+
+	it('handles search functionality', async () => {
+		const { getByPlaceholderText, getByText } = render(
+			<ListModal getItems={mockGetItems} isOpen={true} setIsOpen={() => {}} title="Test Modal" RenderItem={() => <div>Mock Item</div>} />,
+		)
+
+		const searchInput = getByPlaceholderText('Search...')
+
+		fireEvent.change(searchInput, { target: { value: 'test search' } })
+
+		await waitFor(() => {
+			expect(mockGetItems).toHaveBeenCalledWith({
+				offset: 0,
+				limit: 15,
+				search: 'test search',
+			})
+		})
+
+		expect(getByText('Test Modal')).toBeInTheDocument()
+	})
+})
