7ae5d672637a7b40742843d0a043fd94132fc27b
lufaque <95128636+lufaque@users.noreply.github.com>
2023-12-21 13:58:55 +0300

    Revert "fail test fixed"
    This reverts commit f0ff381bcd4d629ebb28f8680bc2ef2a9ae3a474.

---
+++ src/components/buttons/SegmentedControl/SegmentedControl.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import classNames from './SegmentedControl.module.pcss'
+import { Button } from '../Button/Button'
+
+interface SegmentedControlProps {
+	data: Item[]
+	selectedItem: Item
+	setSelectedItem: (item: Item) => void
+}
+
+interface Item {
+	title: string
+	value: string
+}
+
+export const SegmentedControl: FC<SegmentedControlProps> = ({ data, selectedItem, setSelectedItem }) => (
+	<div className={classNames.container}>
+		{data.map((item: Item) => (
+			<Button key={item.value} variant="black" size="sm" className={selectedItem.value === item.value ? classNames.selected : ''} onClick={() => setSelectedItem(item)}>
+				<p className="body1">{item.title}</p>
+			</Button>
+		))}
+	</div>
+)
