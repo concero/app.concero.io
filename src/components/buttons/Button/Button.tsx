91753395e32cab8950669093e36b35e143eb4a03
lufaque <95128636+lufaque@users.noreply.github.com>
2024-01-30 15:13:47 +0300

    feat: swap card dark theme

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react'
 import { type ButtonProps } from './types'
 import { LoadingAnimation } from '../../layout/LoadingAnimation/LoadingAnimation'
 import { getButtonClasses } from './getButtonClasses'
+import classNames from './Button.module.pcss'
 
 export const Button: FC<ButtonProps> = ({
 	size = 'md',
@@ -17,7 +18,7 @@ export const Button: FC<ButtonProps> = ({
 	const buttonClasses = getButtonClasses(size, variant, isLoading, isDisabled, className)
 
 	return (
-		<button className={buttonClasses} onClick={onClick} aria-label={variant + size}>
+		<button className={buttonClasses} id={classNames[variant]} onClick={onClick} aria-label={variant + size}>
 			{isLoading ? <LoadingAnimation /> : leftIcon}
 			{children}
 			{rightIcon}
