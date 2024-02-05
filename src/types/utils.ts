dc43f0cfa431471b95c7af1143229746c3a58973
lufaque <95128636+lufaque@users.noreply.github.com>
2024-02-05 15:41:39 +0300

    fix: MainRouteInfoTags.tsx gas usd price font size

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,6 @@
 export type NestedObj<TObj, TKey extends keyof TObj> = {
 	[key in TKey]: TObj[key] extends Record<string, unknown> ? NestedObj<TObj[key], keyof TObj[key]> : string
 }
+
+export type Nullable<T> = T | null
+export type Optional<T> = T | undefined
