290702a48df863e1ed525b0a535f61b71a5245df
lufaque <95128636+lufaque@users.noreply.github.com>
2024-02-23 20:16:29 +0300

    fix: vercel build

--- src/utils/initPosthog.ts
+++ src/utils/initPosthog.ts
@@ -4,7 +4,7 @@ import { config } from '../constants/config'
 export function initPosthog() {
 	if (process.env.DEVELOPMENT !== 'true') {
 		posthog.init(config.POSTHOG_API_KEY, {
-			api_host: `${config.baseURL}/posthog`,
+			api_host: `${config.baseURL}/ingest`,
 			autocapture: false,
 		})
 
