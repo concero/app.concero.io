0dcbbac95c0a821e2f7fb83c14c85a77d445bc50
Oleg Kirillovich <olegkron@ya.ru>
2024-03-01 20:07:30 +0000

    init testing ethers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import './styles/App.css'
-import { PostHogProvider } from 'posthog-js/react'
 import { WagmiConfig } from 'wagmi'
-import { Navigator } from './Navigator'
 import { wagmiConfig } from './web3/wagmi'
-import { ThemeProvider } from './hooks/themeContext'
-import { SelectionProvider } from './hooks/SelectionContext'
-import { Notifications } from './components/overlays/Notifications/Notifications'
-import { NotificationsProvider } from './hooks/notificationsContext'
-import { DataProvider } from './hooks/DataContext/DataContext'
 import { lazy, useEffect } from 'react'
-import { I18Provider } from './i18n/I18nextProvider'
 import { initPosthog } from './utils/initPosthog'
 import { bigNumberSettings } from './utils/bigNumberSettings'
+import { EthersTest } from './components/screens/EthersTest'
 
 const WalletConnectModal = lazy(
 	async () => await import('./web3/WalletConnectModal').then(module => ({ default: module.WalletConnectModal })),
@@ -24,23 +17,9 @@ function App() {
 	}, [])
 
 	return (
-		<PostHogProvider>
-			<I18Provider>
-				<WagmiConfig config={wagmiConfig}>
-					<DataProvider>
-						<SelectionProvider>
-							<ThemeProvider>
-								<NotificationsProvider>
-									<Notifications />
-									<Navigator />
-									<WalletConnectModal />
-								</NotificationsProvider>
-							</ThemeProvider>
-						</SelectionProvider>
-					</DataProvider>
-				</WagmiConfig>
-			</I18Provider>
-		</PostHogProvider>
+		<WagmiConfig config={wagmiConfig}>
+			<EthersTest />
+		</WagmiConfig>
 	)
 }
 
