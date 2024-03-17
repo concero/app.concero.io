769363a9a476b356b8365b9ce79795f144530732
Oleg Kirillovich <olegkron@ya.ru>
2024-03-17 22:02:34 +0000

    center card

--- src/components/screens/SwapScreen/SwapScreen.tsx
+++ src/components/screens/SwapScreen/SwapScreen.tsx
@@ -1,43 +1,22 @@
-import { type FC, memo } from 'react'
-import { useMediaQuery } from '../../../hooks/useMediaQuery'
+import { memo } from 'react'
 import { withErrorBoundary } from '../../wrappers/WithErrorBoundary'
 import { SwapCard } from '../../cards/SwapCard/SwapCard'
 import { ChartCard } from '../../cards/ChartCard/ChartCard'
 import classNames from './SwapScreen.module.pcss'
-import { TargetInfoCard } from '../../cards/TargetInfoCard/TargetInfoCard'
 import { NewsCard } from '../../cards/NewsCard/NewsCard'
 import { HistoryCard } from '../../cards/HistoryCard/HistoryCard'
 
-export interface ExchangeScreenProps {}
 const History = memo(withErrorBoundary(HistoryCard))
 const Swap = memo(withErrorBoundary(SwapCard))
 const News = memo(withErrorBoundary(NewsCard))
 const Chart = memo(withErrorBoundary(ChartCard))
 
-export const SwapScreen: FC<ExchangeScreenProps> = () => {
-	const isMobile = useMediaQuery('mobile')
-
-	const desktopLayout = (
-		<div className={`row ${classNames.container}`}>
-			<div className={classNames.mainCardStack}>
-				<Chart />
-			</div>
-			<div className={classNames.secondaryCardStack}>
-				<Swap />
-				<TargetInfoCard />
-			</div>
+export const SwapScreen = () => {
+	const newSwapScreenLayout = (
+		<div className={classNames.newSwapCardContainer}>
+			<Swap />
 		</div>
 	)
 
-	const mobileLayout = (
-		<div className={classNames.container}>
-			<div className={classNames.mainCardStack}>
-				<Swap />
-				<Chart />
-				<TargetInfoCard />
-			</div>
-		</div>
-	)
-
-	return <div style={{ width: '100%', height: '100%' }}>{isMobile ? mobileLayout : desktopLayout}</div>
+	return <div className={classNames.newSwapScreenContainer}>{newSwapScreenLayout}</div>
 }
