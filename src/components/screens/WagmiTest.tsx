1c4fe5ae8f5c1e24204d0b6e2b186b19c2ecc7e9
Oleg Kirillovich <olegkron@ya.ru>
2024-03-18 00:55:56 +0000

    upd

--- src/components/screens/WagmiTest.tsx
+++ src/components/screens/WagmiTest.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button } from '../buttons/Button/Button'
 import { WalletButton } from '../layout/Header/WalletButton/WalletButton'
 import { useAccount, useWalletClient } from 'wagmi'
-import { account, config, walletClient } from '../../web3/wagmi'
+import { config, walletClient } from '../../web3/wagmi'
 import { UNISWAP_ROUTER_ABI } from './uniswap_router_abi'
 import { simulateContract } from 'viem/actions'
 
@@ -11,7 +11,7 @@ export const WagmiTest = () => {
 	const wc = useWalletClient()
 	console.log('wc', wc)
 	const { isConnected } = useAccount()
-	console.log(account)
+
 	// const contract = getContract({
 	// 	abi: UNISWAP_ROUTER_ABI,
 	// 	address: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
@@ -140,54 +140,54 @@ export const WagmiTest = () => {
 	// 		slippageTolerance: '0.5',
 	// 	})
 	// }, [])
-
-	useEffect(() => {
-		setRoute({
-			tokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-			poolAddress: '0x11b815efb8f581194ae79006d24e0d814b7697f6',
-			pool: {
-				_id: '65e2269a01994d2405249882',
-				address: '0x11b815efb8f581194ae79006d24e0d814b7697f6',
-				chainId: '1',
-				dex: 'uniswap',
-				feeTier: '0.5',
-				feesUsd: '39761229.6124632267514279243708229',
-				liquidity: '1522393586821495706',
-				tokens: [
-					{
-						_id: '65e2269801994d2405240acc',
-						address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-						chainId: '1',
-						__v: 0,
-						decimals: 18,
-						name: 'Wrapped Ether',
-						price: '0.0000002987822415708207320090743260818667',
-						symbol: 'WETH',
-						totalValueLocked: '594765.046756222469079695',
-						totalValueLockedUsd: '2037591929.069855110603709326947184',
-						volume: '433603345.943983665069881432',
-						volumeUsd: '935453668493.6076121510907705884333',
-					},
-					{
-						_id: '65e2269801994d2405240ad7',
-						address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-						chainId: '1',
-						__v: 0,
-						decimals: 6,
-						name: 'Tether USD',
-						price: '3.012297746168447421953988421294725',
-						symbol: 'USDT',
-						totalValueLocked: '222925200.540408',
-						totalValueLockedUsd: '222925200.540408',
-						volume: '224830862110.25747',
-						volumeUsd: '225149800348.6348098843671329241243',
-					},
-				],
-				totalValueLockedUsd: '72628229.5017658665677300766062707',
-				volumeUsd: '79522459224.92645350285584874164405',
-			},
-		})
-	}, [])
+	//
+	// useEffect(() => {
+	// 	setRoute({
+	// 		tokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+	// 		poolAddress: '0x11b815efb8f581194ae79006d24e0d814b7697f6',
+	// 		pool: {
+	// 			_id: '65e2269a01994d2405249882',
+	// 			address: '0x11b815efb8f581194ae79006d24e0d814b7697f6',
+	// 			chainId: '1',
+	// 			dex: 'uniswap',
+	// 			feeTier: '0.5',
+	// 			feesUsd: '39761229.6124632267514279243708229',
+	// 			liquidity: '1522393586821495706',
+	// 			tokens: [
+	// 				{
+	// 					_id: '65e2269801994d2405240acc',
+	// 					address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+	// 					chainId: '1',
+	// 					__v: 0,
+	// 					decimals: 18,
+	// 					name: 'Wrapped Ether',
+	// 					price: '0.0000002987822415708207320090743260818667',
+	// 					symbol: 'WETH',
+	// 					totalValueLocked: '594765.046756222469079695',
+	// 					totalValueLockedUsd: '2037591929.069855110603709326947184',
+	// 					volume: '433603345.943983665069881432',
+	// 					volumeUsd: '935453668493.6076121510907705884333',
+	// 				},
+	// 				{
+	// 					_id: '65e2269801994d2405240ad7',
+	// 					address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+	// 					chainId: '1',
+	// 					__v: 0,
+	// 					decimals: 6,
+	// 					name: 'Tether USD',
+	// 					price: '3.012297746168447421953988421294725',
+	// 					symbol: 'USDT',
+	// 					totalValueLocked: '222925200.540408',
+	// 					totalValueLockedUsd: '222925200.540408',
+	// 					volume: '224830862110.25747',
+	// 					volumeUsd: '225149800348.6348098843671329241243',
+	// 				},
+	// 			],
+	// 			totalValueLockedUsd: '72628229.5017658665677300766062707',
+	// 			volumeUsd: '79522459224.92645350285584874164405',
+	// 		},
+	// 	})
+	// }, [])
 
 	return (
 		<div>
