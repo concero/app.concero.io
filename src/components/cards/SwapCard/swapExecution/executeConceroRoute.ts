17c2f7834f07f906cdb6fa1aa0c0054f56c57a28
lufaque <95128636+lufaque@users.noreply.github.com>
2024-03-18 15:04:56 +0300

    feat: setup event listener

--- src/components/cards/SwapCard/swapExecution/executeConceroRoute.ts
+++ src/components/cards/SwapCard/swapExecution/executeConceroRoute.ts
@@ -1,3 +1,11 @@
+import { publicClient } from '../../../../web3/wagmi'
+
 export async function executeConceroRoute() {
 	console.log('Executing concero route')
+	const unwantch = publicClient.watchEvent({
+		address: '0x7388365d49049164B6aBF3218c8cbA67A51580EF',
+		onLogs: logs => {
+			console.log('Logs:', logs)
+		},
+	})
 }
