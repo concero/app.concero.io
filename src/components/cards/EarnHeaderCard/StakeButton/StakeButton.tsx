bac6035bbd5b2b328b7fa1c3450d76d1e66573ca
lufaque <95128636+lufaque@users.noreply.github.com>
2024-05-21 13:21:32 +0300

    fix: build

--- src/components/cards/EarnHeaderCard/StakeButton/StakeButton.tsx
+++ src/components/cards/EarnHeaderCard/StakeButton/StakeButton.tsx
@@ -1,39 +1,31 @@
 import { type Dispatch } from 'react'
-import { useSwitchNetwork } from 'wagmi'
-import { Button } from '../../../buttons/Button/Button'
-import classNames from './StakeButton.module.pcss'
-import { handleExecuteSwap } from '../ManageModal/swapExecution/handleExecuteSwap'
 import { type ManageState } from '../ManageModal/useEarnReducer/types'
-import { buttonMessages, Status } from '../ManageModal/constants'
-import { buttonClassNames, buttonIcons } from './styleHandlers'
-import { type SwitchNetworkArgs, type SwitchNetworkResult } from '@wagmi/core'
-import { useTranslation } from 'react-i18next'
 
 interface StakeButtonProps {
 	manageState: ManageState
 	manageDispatch: Dispatch<any>
 }
 
-type SwitchChainNetwork = (chainId_?: SwitchNetworkArgs['chainId']) => Promise<SwitchNetworkResult>
+// type SwitchChainNetwork = (chainId_?: SwitchNetworkArgs['chainId']) => Promise<SwitchNetworkResult>
 
 export function StakeButton({ manageState, manageDispatch }: StakeButtonProps) {
-	const { switchNetworkAsync } = useSwitchNetwork()
-	const { status } = manageState
-	const isDisabled = status !== Status.stake && status !== Status.withdraw
-	const { t } = useTranslation()
-
-	return (
-		<Button
-			leftIcon={buttonIcons[status as Status]}
-			size="lg"
-			isLoading={manageState.isLoading}
-			className={classNames[buttonClassNames(status)]}
-			onClick={async () => {
-				await handleExecuteSwap(manageState, manageDispatch, switchNetworkAsync as SwitchChainNetwork)
-			}}
-			isDisabled={isDisabled}
-		>
-			{t(buttonMessages[status as Status])}
-		</Button>
-	)
+	// const { switchNetworkAsync } = useSwitchNetwork()
+	// const { status } = manageState
+	// const isDisabled = status !== Status.stake && status !== Status.withdraw
+	// const { t } = useTranslation()
+	//
+	// return (
+	// 	<Button
+	// 		leftIcon={buttonIcons[status as Status]}
+	// 		size="lg"
+	// 		isLoading={manageState.isLoading}
+	// 		className={classNames[buttonClassNames(status)]}
+	// 		onClick={async () => {
+	// 			await handleExecuteSwap(manageState, manageDispatch, switchNetworkAsync as SwitchChainNetwork)
+	// 		}}
+	// 		isDisabled={isDisabled}
+	// 	>
+	// 		{t(buttonMessages[status as Status])}
+	// 	</Button>
+	// )
 }
