bd8c4205fd429e84f44ef7a543f706c4f7d9d62c
kituwasthere <01n.kuznetsov@gmail.com>
2024-08-07 19:43:04 +0300

    add: fix quests

--- src/api/concero/quest/fetchQuests.ts
+++ src/api/concero/quest/fetchQuests.ts
@@ -4,15 +4,15 @@ import { type IQuest } from './questType'
 export const fetchQuests = async (): Promise<IQuest[]> => {
 	const url = `${process.env.CONCERO_API_URL}/quests`
 
-	const token = process.env.QUESTS_AUTH_TOKEN
+	// const token = process.env.QUESTS_AUTH_TOKEN
+	//
+	// const response = await get(url, {
+	// 	headers: {
+	// 		Authorization: `Bearer ${token}`,
+	// 	},
+	// })
 
-	const response = await get(url, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-
-	// const response = await get(url)
+	const response = await get(url)
 	if (response.status !== 200) throw new Error('Something went wrong')
 	return response.data.data
 }
