31acf7cbfac32facf29c18b23973e41138de4031
andrej-concero <andrej@concero.io>
2024-12-02 10:42:36 +0100

    Update wallet connection package
    Update the wallet connection pkg from web3modal
    to reown app kit. Change the Wagmi congifuration
    with a Wagmi Adapter. Substitute the useWeb3Modal
    hook with useAppKit hook in components that use it

--- src/web3/wagmi.ts
+++ src/web3/wagmi.ts
@@ -1,4 +1,4 @@
-import { createConfig, http } from 'wagmi'
+import { http } from 'wagmi'
 import { fallback } from 'viem'
 import {
 	arbitrum,
@@ -25,15 +25,18 @@ import {
 	polygonZkEvm,
 	sepolia,
 	zkSync,
-} from 'wagmi/chains'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
-import { createWeb3Modal } from '@web3modal/wagmi/react'
+} from '@reown/appkit/networks'
 import { config as appConfig } from '../constants/config'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { createAppKit } from '@reown/appkit/react'
+import { type AppKitNetwork } from '@reown/appkit/networks'
 
 export const projectId = appConfig.WEB3_MODAL_PROJECT_ID
-export const chains = [
+
+const chains: [AppKitNetwork, ...AppKitNetwork[]] = [
 	mainnet,
 	polygon,
+	sepolia,
 	polygonZkEvm,
 	arbitrum,
 	aurora,
@@ -52,13 +55,11 @@ export const chains = [
 	cronos,
 	linea,
 	evmos,
-	sepolia,
 	baseSepolia,
 	arbitrumSepolia,
 	optimismSepolia,
 ]
 
-// 2. Create wagmiConfig
 const metadata = {
 	name: 'Concero',
 	description: 'Concero',
@@ -66,94 +67,74 @@ const metadata = {
 	icons: ['https://avatars.githubusercontent.com/u/37784886'],
 }
 
-export const config = createConfig({
-	chains: [
-		mainnet,
-		polygon,
-		sepolia,
-		polygonZkEvm,
-		arbitrum,
-		aurora,
-		zkSync,
-		moonriver,
-		moonbeam,
-		boba,
-		optimism,
-		fuse,
-		bsc,
-		avalanche,
-		gnosis,
-		base,
-		fantom,
-		okc,
-		cronos,
-		linea,
-		evmos,
-		baseSepolia,
-		arbitrumSepolia,
-		optimismSepolia,
-	],
-	transports: {
-		[mainnet.id]: http(),
-		[sepolia.id]: http(),
-		[polygon.id]: fallback([
-			http('https://polygon.publicnode.com'),
-			http('https://polygon.drpc.org'),
-			http('https://rpc.ankr.com/polygon'),
-			http('https://public.stackup.sh/api/v1/node/polygon-mainnet'),
-		]),
-		[polygonZkEvm.id]: http(),
-		[arbitrum.id]: fallback([
-			http('https://arb1.arbitrum.io/rpc'),
-			http('https://arbitrum.public-rpc.com'),
-			http('https://arb1.arbitrum.io/rpc'),
-			http(),
-		]),
-		[aurora.id]: http(),
-		[zkSync.id]: http(),
-		[moonriver.id]: http(),
-		[moonbeam.id]: http(),
-		[boba.id]: http(),
-		[optimism.id]: http(),
-		[fuse.id]: http(),
-		[bsc.id]: http(),
-		[avalanche.id]: fallback([
-			http('https://api.avax.network/ext/bc/C/rpc'),
-			http('https://rpc.ankr.com/avalanche'),
-			http('https://avalanche.public-rpc.com'),
-			http(),
-		]),
-		[gnosis.id]: http(),
-		[base.id]: fallback([
-			http('https://base-rpc.publicnode.com'),
-			http('https://base.blockpi.network/v1/rpc/public'),
-			http('https://public.stackup.sh/api/v1/node/base-mainnet'),
-			http(),
-			http('https://mainnet.base.org'),
-		]),
-		[fantom.id]: http(),
-		[okc.id]: http(),
-		[cronos.id]: http(),
-		[linea.id]: http(),
-		[evmos.id]: http(),
-		[sepolia.id]: http(),
-		[baseSepolia.id]: http(),
-		[arbitrumSepolia.id]: http('https://arbitrum-sepolia-rpc.publicnode.com'),
-		[optimismSepolia.id]: http(),
-	},
-	connectors: [
-		walletConnect({ projectId, metadata, showQrModal: false }),
-		injected({ shimDisconnect: true }),
-		coinbaseWallet({
-			appName: metadata.name,
-			appLogoUrl: metadata.icons[0],
-		}),
-	],
+const createTransports = () => ({
+	[mainnet.id]: http(),
+	[polygon.id]: fallback([
+		http('https://polygon.publicnode.com'),
+		http('https://polygon.drpc.org'),
+		http('https://rpc.ankr.com/polygon'),
+		http('https://public.stackup.sh/api/v1/node/polygon-mainnet'),
+	]),
+	[polygonZkEvm.id]: http(),
+	[arbitrum.id]: fallback([
+		http('https://arb1.arbitrum.io/rpc'),
+		http('https://arbitrum.public-rpc.com'),
+		http('https://arb1.arbitrum.io/rpc'),
+		http(),
+	]),
+	[aurora.id]: http(),
+	[zkSync.id]: http(),
+	[moonriver.id]: http(),
+	[moonbeam.id]: http(),
+	[boba.id]: http(),
+	[optimism.id]: http(),
+	[fuse.id]: http(),
+	[bsc.id]: http(),
+	[avalanche.id]: fallback([
+		http('https://api.avax.network/ext/bc/C/rpc'),
+		http('https://rpc.ankr.com/avalanche'),
+		http('https://avalanche.public-rpc.com'),
+		http(),
+	]),
+	[gnosis.id]: http(),
+	[base.id]: fallback([
+		http('https://base-rpc.publicnode.com'),
+		http('https://base.blockpi.network/v1/rpc/public'),
+		http('https://public.stackup.sh/api/v1/node/base-mainnet'),
+		http(),
+		http('https://mainnet.base.org'),
+	]),
+	[fantom.id]: http(),
+	[okc.id]: http(),
+	[cronos.id]: http(),
+	[linea.id]: http(),
+	[evmos.id]: http(),
+	[sepolia.id]: http(),
+	[baseSepolia.id]: http(),
+	[arbitrumSepolia.id]: http('https://arbitrum-sepolia-rpc.publicnode.com'),
+	[optimismSepolia.id]: http(),
 })
 
-// 3. Create modal
-createWeb3Modal({
-	wagmiConfig: config,
+const transports = createTransports()
+
+export const wagmiAdapter = new WagmiAdapter({
+	networks: chains,
+	transports,
 	projectId,
-	enableAnalytics: true,
 })
+
+createAppKit({
+	adapters: [wagmiAdapter],
+	networks: chains,
+	metadata,
+	projectId,
+	features: {
+		socials: false,
+		analytics: true,
+		email: false,
+		onramp: false,
+		swaps: false,
+	},
+})
+
+export const config = wagmiAdapter.wagmiConfig
