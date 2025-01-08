2bee2bd6ee9542c3736acc1a56e0b324f6af8666
babamovandrej <andrejbabamov@gmail.com>
2025-01-08 16:46:50 +0100

    Fix deposit button state in swap input card

--- src/components/pool/PoolCard/SwapButton/SwapButton.tsx
+++ src/components/pool/PoolCard/SwapButton/SwapButton.tsx
@@ -3,14 +3,22 @@ import { useAppKit } from '@reown/appkit/react'
 import { useAccount } from 'wagmi'
 import { Button } from '../../../buttons/Button/Button'
 
-export const SwapButton = ({ error, onClick, isLoading, isDeposit }: SwapButtonProps) => {
+export const SwapButton = ({ onClick, isLoading, isDeposit, isDepositDisabled }: SwapButtonProps) => {
 	const { isConnected } = useAccount()
 	const { open } = useAppKit()
 
 	const actionText: string = isDeposit ? 'Request Deposit' : 'Request Withdrawal'
+	const isDisabled: boolean = (isDeposit && isDepositDisabled) || !isConnected
 
 	return (
-		<Button isFull size="lg" variant="primary" isLoading={isLoading} onClick={isConnected ? onClick : open}>
+		<Button
+			isFull
+			size="lg"
+			variant="primary"
+			isLoading={isLoading}
+			onClick={isConnected ? e => onClick(e) : () => open()}
+			isDisabled={isDisabled}
+		>
 			{isConnected ? actionText : 'Connect Wallet'}
 		</Button>
 	)
