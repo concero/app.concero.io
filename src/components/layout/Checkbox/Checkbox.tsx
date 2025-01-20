8547c8687b60f00f87e00f0521694bfae8635ccc
mikhail@concero.io <mikhail@concero.io>
2025-01-20 12:40:06 +0300

    Added support label in the Checkbox

--- src/components/layout/Checkbox/Checkbox.tsx
+++ src/components/layout/Checkbox/Checkbox.tsx
@@ -1,19 +1,23 @@
-import { type FC, useState, type MouseEvent, HTMLProps } from 'react'
+import { type FC, useState, type MouseEvent, HTMLProps, ReactNode, useId, MouseEventHandler } from 'react'
 import cls from './Checkbox.module.pcss'
 import { classNames } from '../../../utils/classNames/classNames'
 
-type TProps = {
+type TProps = HTMLProps<HTMLDivElement> & {
 	isChecked?: boolean
 	onChange?: (isChecked: boolean) => void
 	className?: string
 	disabled?: boolean
-} & HTMLProps<HTMLDivElement>
+	label?: string | number
+	id?: string | number
+}
 
 export const Checkbox = (props: TProps) => {
-	const { className, disabled, isChecked, onChange } = props
+	const { className, disabled, isChecked, onChange, id, label, ...otherProps } = props
 	const [internalChecked, setInternalChecked] = useState(false)
+	const checkboxGeneratedId = useId()
+	const checkboxId = id ?? checkboxGeneratedId
 
-	const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+	const handleClick = (event: MouseEvent) => {
 		if (disabled) return
 		const nextChecked = isChecked ?? !internalChecked
 		setInternalChecked(!internalChecked)
@@ -21,22 +25,35 @@ export const Checkbox = (props: TProps) => {
 	}
 
 	return (
-		<div
-			className={classNames(
-				cls.checkbox,
-				{
-					[cls.checked]: isChecked ?? internalChecked,
-					[cls.disabled]: disabled,
-				},
-				[className],
-			)}
-			aria-disabled={disabled}
-			onClick={handleClick}
-			tabIndex={0}
-			role="checkbox"
-			aria-checked={isChecked ?? internalChecked}
-		>
-			<div className={cls.inner}></div>
+		<div className={classNames(cls.wrap_with_label)}>
+			<div
+				className={classNames(
+					cls.checkbox,
+					{
+						[cls.checked]: isChecked ?? internalChecked,
+						[cls.disabled]: disabled,
+					},
+					[className],
+				)}
+				aria-disabled={disabled}
+				onClick={handleClick}
+				tabIndex={0}
+				role="checkbox"
+				aria-checked={isChecked ?? internalChecked}
+				id={checkboxId}
+				{...otherProps}
+			>
+				<div className={cls.inner}></div>
+			</div>
+			{label ? (
+				<label
+					onClick={handleClick}
+					htmlFor={checkboxId}
+					className={classNames(cls.label, { [cls.label_disabled]: disabled }, [])}
+				>
+					{label}
+				</label>
+			) : null}
 		</div>
 	)
 }
