0a65b0d35e65c99702e373167a6d422595f0fa64
mikhail@concero.io <mikhail@concero.io>
2025-03-14 19:17:32 +0300

    reset userAction

--- src/api/concero/userActions/fetchUserActions.ts
+++ src/api/concero/userActions/fetchUserActions.ts
@@ -2,7 +2,7 @@ import { TApiResponse } from '@/types/api'
 import { get } from '../../client'
 import { type IUserAction } from './userActionType'
 
-export const fetchUserActions = async (address: string, { page, limit }: TPaginationParams) => {
+export const fetchUserActions = async (address: string) => {
 	const url = `${process.env.CONCERO_API_URL}/userActions/${address}`
 
 	const response = await get<TApiResponse<IUserAction[]>>(url)
