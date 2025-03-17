76d51ce494fca83cf388fea7e5684674235c33b6
mikhail@concero.io <mikhail@concero.io>
2025-03-17 18:19:29 +0300

    update list admin addresses

--- src/shared/lib/tests/isAdminAddress.ts
+++ src/shared/lib/tests/isAdminAddress.ts
@@ -10,6 +10,9 @@ const ADMIN_ADDRESSES: string[] = Array.from(
 		'0xdddd5f804b9d293dce8819d232e8d76381605a62',
 		'0xe129d765458ca35b6406fa6cb3a7eca847696268',
 		'0x55ad9b67a054d8ee6ffb1bae26555431d76d3ef6',
+		'0x546060b9f58182ad21fba50bf78e71eaf3925b32',
+		'0x0e71d5cff90abacd07be4f79ff245984e73edb08',
+		'0x207da52a19bdde899f200989c04b4f62ad5176ef',
 	]),
 ).map(addr => addr.toLowerCase().trim())
 
