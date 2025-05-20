fd65bb0a3a3988e6e9e1413eac5c716d694b30cc
mikhail@concero.io <mikhail@concero.io>
2025-05-20 12:41:38 +0300

    update discord link

--- src/entities/User/config/consts/discordLink.ts
+++ src/entities/User/config/consts/discordLink.ts
@@ -1,8 +1,6 @@
-// export const DISCORD_LINK_AUTH = `https://discord.com/oauth2/authorize?client_id=1267215033025429595&response_type=code&redirect_uri=https%3A%2F%2Fapp.concero.io%2Fprofile&scope=identify+guilds+email`
-
 const clientId = '1267215033025429595'
 const redirectUri = 'https://app.concero.io/profile'
 
 export const DISCORD_LINK_AUTH = `https://discord.com/oauth2/authorize?client_id= ${clientId}&response_type=code&redirect_uri=${encodeURIComponent(
 	redirectUri,
-)}&scope=identify%20guilds%20email`
+)}&scope=identify+guilds+email`
