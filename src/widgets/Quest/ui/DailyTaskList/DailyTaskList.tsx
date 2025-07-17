a9c2d7cbd5ee290e1ced3b23286e89cfcd13d943
mikhail@concero.io <mikhail@concero.io>
2025-07-17 15:19:22 +0300

    tesntet only for admin

--- src/widgets/Quest/ui/DailyTaskList/DailyTaskList.tsx
+++ src/widgets/Quest/ui/DailyTaskList/DailyTaskList.tsx
@@ -3,12 +3,13 @@ import { useAccount } from 'wagmi'
 import { useUserByAddress } from '@/entities/User'
 import { QuestPreviewItem } from '../QuestPreviewItem/QuestPreviewItem'
 import { useAllQuests } from '@/entities/Quest'
+import { isAdminAddress } from '@/shared/lib/tests/isAdminAddress'
 export const DailyTaskList = (): JSX.Element => {
 	const { data: quests } = useAllQuests()
 	const account = useAccount()
 	const { data: user } = useUserByAddress(account.address)
 	const DailyQuests = quests
-		?.filter(q => q.type == 'Daily')
+		?.filter(q => q.type == 'Daily' && (q.name === 'Testnet Daily' ? isAdminAddress(account.address) : false))
 		.toSorted((a, b) => {
 			return (b?.priority || 0) - (a?.priority || 0)
 		})
