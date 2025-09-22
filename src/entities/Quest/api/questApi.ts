6202ff55415018b886088184044a6d3868fb63b5
mikhail@concero.io <mikhail@concero.io>
2025-09-22 16:35:50 +0300

    change to 10 items

--- src/entities/Quest/api/questApi.ts
+++ src/entities/Quest/api/questApi.ts
@@ -7,7 +7,7 @@ import { queryClient } from '@/shared/api/tanstackClient'
 const questService = {
 	getAllQuests: async () => {
 		const url = `${process.env.CONCERO_API_URL}/quest`
-		const { payload } = await get<TApiResponse<TGetAllQuestsResponse>>(url, { skip: 0, take: 5 })
+		const { payload } = await get<TApiResponse<TGetAllQuestsResponse>>(url, { skip: 0, take: 10 })
 		return payload
 	},
 	getUserQuests: async ({
