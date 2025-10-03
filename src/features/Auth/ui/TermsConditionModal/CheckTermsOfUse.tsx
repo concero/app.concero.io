0df803dccd421d10f874bbd89896edf8a7380d34
mikhail@concero.io <mikhail@concero.io>
2025-10-03 20:48:12 +0300

    upate invalidate queries

--- src/features/Auth/ui/TermsConditionModal/CheckTermsOfUse.tsx
+++ src/features/Auth/ui/TermsConditionModal/CheckTermsOfUse.tsx
@@ -7,6 +7,7 @@ import { verifyUser } from './model/lib/verifyUser'
 import { CheckTermsModalProvider, useCheckTermsModal } from './CheckTermsModalContext'
 import { useAcceptTermsMutation, useUserByAddress } from '@/entities/User/api/userApi'
 import { Http } from '@/shared/types/api'
+import { queryClient } from '@/shared/api/tanstackClient'
 
 const CheckTermsOfUseDecoratorInner = ({ children }: PropsWithChildren) => {
 	const { address, isConnected } = useAccount()
@@ -39,6 +40,13 @@ const CheckTermsOfUseDecoratorInner = ({ children }: PropsWithChildren) => {
 		}
 		setIsLoadingTerms(true)
 		verifyUser({ address, signMessageAsync, acceptTerms })
+			.then(() => {
+				queryClient.resetQueries()
+
+				queryClient.refetchQueries({
+					type: 'all',
+				})
+			})
 			.catch((err: any) => {
 				console.error(`CheckTermsOfUseDecoratorInner | verfiyUser process with error`, err)
 
