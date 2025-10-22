64f3a35b15de68699605fc0f4ae39d799824fa42
mikhail@concero.io <mikhail@concero.io>
2025-10-22 17:13:30 +0300

    Open the Reward page

--- src/shared/consts/config/config.ts
+++ src/shared/consts/config/config.ts
@@ -32,7 +32,7 @@ export const configEnvs = {
 
 	// POOLS & LP
 	POOL_IS_NOT_AVAILABLE: false,
-	REWARD_IS_NOT_AVAILABLE: true,
+	REWARD_IS_NOT_AVAILABLE: false,
 	PROFILE_IS_NOT_AVAILABLE: false,
 
 	PARENT_POOL_CONTRACT: IS_TESTNET ? parentPoolBaseSepolia : parentPoolBase,
