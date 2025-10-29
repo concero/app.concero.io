446f5301ff13848fb259644e1dc26f16bf9a7142
mikhail@concero.io <mikhail@concero.io>
2025-10-29 17:30:48 +0300

    update width of streak tooltips

--- src/widgets/Header/ui/UserMultipliers/UserMultipliers.tsx
+++ src/widgets/Header/ui/UserMultipliers/UserMultipliers.tsx
@@ -50,27 +50,3 @@ export const UserMultipliers = ({ user }: { user: TUserResponse }) => {
 		</VStack>
 	)
 }
-// <VStack gap="space_0_75" justify="between" className={cls.user_multipliers}>
-// 	<HStack justify="between" className={cls.multiplier_heading}>
-// 		<Text variant="heading_medium" className={cls.heading}>
-// 			Multipliers
-// 		</Text>
-// 		<Text variant="heading_medium" className={cls.multiplier_value}>
-// 			{totalMultiplier}x
-// 		</Text>
-// 	</HStack>
-// 	<VStack gap="space_0_5" className={cls.multiplier_components}>
-// 		<HStack justify="between" className={cls.item}>
-// 			<Text variant="body_medium">Base</Text>
-// 			<Text variant="heading_small">{base || 0}x</Text>
-// 		</HStack>
-// <HStack justify="between" className={cls.item}>
-// 	<Text variant="body_medium">Liquidity Holding</Text>
-// 	<Text variant="heading_small">{liquidity_pool || 0}x</Text>
-// </HStack>
-// <HStack justify="between" className={cls.item}>
-// 	<Text variant="body_medium">Daily Swapping</Text>
-// 	<Text variant="heading_small">{daily_swaps || 0}x</Text>
-// </HStack>
-// 	</VStack>
-// </VStack>
