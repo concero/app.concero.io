91c74fd8c56937440ea4526cdf789d0c92e215c3
mikhail@concero.io <mikhail@concero.io>
2026-02-18 17:33:28 +0300

    remove extra files

--- src/api/concero/fetchTransactionsCount.ts
+++
@@ -1,9 +0,0 @@
-import { get } from '../client'
-
-export const fetchTransactionsCount = async (): Promise<number> => {
-	const url = `${process.env.CONCERO_API_URL}/bridgeTransactionsCount`
-
-	const response = await get(url)
-	if (response.status !== 200) throw new Error('No transactions found')
-	return response.data.data
-}
--- src/api/concero/rewards/loyaltyReward/checkLoyaltyBonus.ts
+++
@@ -1,9 +0,0 @@
-import { get } from '../../../client'
-
-export const checkLoyaltyBonus = async (address: string): Promise<boolean> => {
-	const url = `${process.env.CONCERO_API_URL}/rewards/checkLoyaltyBonus/${address}`
-
-	const response = await get(url)
-	if (response.status !== 200) throw new Error('Something went wrong')
-	return response.data.data
-}
