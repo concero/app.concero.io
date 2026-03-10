63e36ef568b666dd2b90aea57f716f64361b606a
mikhail@concero.io <mikhail@concero.io>
2026-03-10 16:28:29 +0300

    update streak banner

--- src/entities/Social/ui/Banners/StreaksBanner/StreaksBanner.tsx
+++ src/entities/Social/ui/Banners/StreaksBanner/StreaksBanner.tsx
@@ -1,5 +1,5 @@
-import { Banner } from '@/shared/ui'
-import InfoIcon from '@/shared/assets/icons/InfoWrapped.svg?react'
+import { Banner, Text } from '@/shared/ui'
+import CersIcon from '@/shared/assets/icons/CersEidi.svg?react'
 import cls from './StreaksBanner.module.pcss'
 
 export const StreaksBanner = () => {
@@ -7,12 +7,14 @@ export const StreaksBanner = () => {
 		<Banner className={cls.cerp_banner_wrap}>
 			<div className={cls.heading_wrap_with_icon}>
 				<div className={cls.wrap_icon}>
-					<InfoIcon />
+					<CersIcon />
 				</div>
 				<div className={cls.heading_wrap}>
-					<span className={cls.title}>
-						Streaks will no longer accumulate but won’t reset — multipliers remain intact.
-					</span>
+					<span className={cls.title}>CERs season 2 is here!</span>
+					<Text variant="body_large" className={cls.description}>
+						Streaks will not accumulate temporarily, but they will not be reset either — multipliers will
+						remain unchanged.
+					</Text>
 				</div>
 			</div>
 		</Banner>
